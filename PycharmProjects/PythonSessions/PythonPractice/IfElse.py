13b5d0e30559bd61bfd331bda3aa9cd83160f2bb
ArjunRao1995 <arjunsrao18@gmail.com>
2021-01-07 09:39:29 +0530

    Committing IfElse code

---
+++ PycharmProjects/PythonSessions/PythonPractice/IfElse.py
@@ -0,0 +1,9 @@
+x=int(input("Enter value of x"))
+if(x<0):
+    print("x is negative number")
+elif(x>0):
+    print("x is positive number")
+elif(x==0):
+    print("x is equal to Zero")
+else:
+    print("x is Indefinite number")
