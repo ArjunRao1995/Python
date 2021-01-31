da769917dcd5b599e3bf2c2e3923316fadf699e0
ArjunRao1995 <arjunsrao18@gmail.com>
2021-01-31 15:01:52 +0530

    IfElse

--- PycharmProjects/PythonSessions/PythonPractice/IfElse.py
+++ PycharmProjects/PythonSessions/PythonPractice/IfElse.py
@@ -7,3 +7,14 @@ elif(x==0):
     print("x is equal to Zero")
 else:
     print("x is Indefinite number")
+
+#To find the highest number
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=int(input("Enter the value of c"))
+if(a>b and a>c):
+    print("a is highest number")
+elif(b>c):
+    print("b is highest number")
+else:
+    print("c is the highest number")
